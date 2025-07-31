e49043a2320431d17faa62ef0bc81a47f505566c
Andres <a.ge9411@gmail.com>
2025-07-30 19:48:58 -0600

    vista contact blade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +23,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/contact', fn() =>  Response::view('contact'));
+Route::post('/contact', function (Request $request) {
+    dd($request->get('phone_number'));
+});
